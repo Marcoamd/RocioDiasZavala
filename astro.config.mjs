4950386f67345216bb8d9c729d6855e164d811dd
Paul M. Yncaluque <yncaluquedev@gmail.com>
2023-11-05 19:11:11 -0500

    changes and test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
+  site: "https://Marcoamd.github.io",
+  base: "/RocioDiasZavala",
 });
