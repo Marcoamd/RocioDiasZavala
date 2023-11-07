2c7f7fc90fb0e5fe6c94cc7e702132702b54f3c6
Paul M. Yncaluque <yncaluquedev@gmail.com>
2023-11-06 19:32:30 -0500

    menu width

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
   site: "https://marcoamd.github.io",
   base: "/RocioDiasZavala",
 });
